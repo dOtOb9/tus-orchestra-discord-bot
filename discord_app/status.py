aaaa8f5a0b047ae960dddebafc8d01f9111bcbcb
dOtOb9 <masa.taga0528@gmail.com>
2024-08-13 23:57:44 +0900

    出欠状況ボタンURLを修正

--- discord_app/status.py
+++ discord_app/status.py
@@ -1,6 +1,10 @@
+from os import getenv
+
 import discord
 
 
 class UserStatusButton(discord.ui.Button):
     def __init__(self, disabled: bool = False, row: int = None) -> None:
-        super().__init__(label="出席状況", style=discord.ButtonStyle.gray, row=row, disabled=disabled, url="https://docs.pycord.dev/en/stable/api/ui_kit.html#discord.ui.Button.url")
+        redirect_url = getenv("SPREADSHEET_EXEC_URL") + "?mode=dashboard"
+
+        super().__init__(label="出席状況", style=discord.ButtonStyle.gray, row=row, disabled=disabled, url=redirect_url)
