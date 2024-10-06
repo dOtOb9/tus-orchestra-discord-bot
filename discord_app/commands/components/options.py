67932aed3e7c4fa02a2b84fe426a9f822d50584f
dOtOb9 <masa.taga0528@gmail.com>
2024-10-06 09:41:09 +0900

    kwargsの削除

--- discord_app/commands/components/options.py
+++ discord_app/commands/components/options.py
@@ -2,32 +2,32 @@ import discord
 
 
 class YearOption(discord.Option):
-    def __init__(self, **kwargs):
-        super().__init__(int, min_value=2024, max_value=2100, description=kwargs['add_desc'] + "何年かを入力してください。", **kwargs)
+    def __init__(self, add_desc):
+        super().__init__(int, min_value=2024, max_value=2100, description=add_desc + "何年かを入力してください。")
 
 class MonthOption(discord.Option):
-    def __init__(self, **kwargs):
-        super().__init__(int, max_value=12, min_value=1, description=kwargs['add_desc'] + "何月かを入力してください。", **kwargs)
+    def __init__(self, add_desc):
+        super().__init__(int, max_value=12, min_value=1, description=add_desc + "何月かを入力してください。")
 
 
 class DayOption(discord.Option):
-    def __init__(self, **kwargs):
-        super().__init__(int, min_value=1, max_value=31, description=kwargs['add_desc'] + "何日かを入力してください。", **kwargs)
+    def __init__(self, add_desc):
+        super().__init__(int, min_value=1, max_value=31, description=add_desc + "何日かを入力してください。")
 
 
 class HourOption(discord.Option):
-    def __init__(self, **kwargs):
-        super().__init__(int, max_value=23, min_value=0, description=kwargs['add_desc'] + "何時かを入力してください。", **kwargs)
+    def __init__(self, add_desc):
+        super().__init__(int, max_value=23, min_value=0, description=add_desc + "何時かを入力してください。")
 
 
 class MinuteOption(discord.Option):
-    def __init__(self, **kwargs):
-        super().__init__(int, min_value=0, max_value=59, description=kwargs['add_desc'] + "何分かを入力してください。", **kwargs)
+    def __init__(self, add_desc):
+        super().__init__(int, min_value=0, max_value=59, description=add_desc + "何分かを入力してください。")
 
 
 class SendTypeOption(discord.Option):
-    def __init__(self, **kwargs):
-        super().__init__(str, choices=["Cc", "Bcc"], description="送信先を表示可とする場合は`Cc`、それ以外は`Bcc`と入力してください。", **kwargs)
+    def __init__(self, add_desc):
+        super().__init__(str, choices=["Cc", "Bcc"], description="送信先を表示可とする場合は`Cc`、それ以外は`Bcc`と入力してください。")
 
 
 class CampusOption(discord.Option):
@@ -36,7 +36,7 @@ class CampusOption(discord.Option):
 
 class TrainingOption(discord.Option):
     def __init__(self):
-        super().__init__(str, choices=["前曲", "中曲", "メイン曲", "無し"], description="練習曲を選択してください。")
+        super().__init__(str, choices=["無し", "前曲", "中曲", "メイン１", "メイン２", "メイン３", "メイン４"], description="練習曲を選択してください。")
 
 class SectionOption(discord.Option):
     def __init__(self):
